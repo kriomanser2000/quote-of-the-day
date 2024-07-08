e856b408866035b90524f28acb233385effcf03d
Kriomanser2000 <kuzmenkasana05@gmail.com>
2024-07-08 18:14:36 +0300

    i

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
-function App() {
+function App()
+{
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const fetchQuote = async () => 
+  {
+    try 
+    {
+      const response = await axios.get('https://api.quotable.io/random');
+      setQuote(response.data.content);
+      setAuthor(response.data.author);
+    } catch (error) 
+    {
+      console.error("Error fetching the quote", error);
+      setQuote('Could not fetch the quote. Please try again later.');
+      setAuthor('');
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quote of the Day</h1>
+        <button onClick={fetchQuote}>Get Quote</button>
+        {quote && (
+          <div>
+            <p>"{quote}"</p>
+            <p><em>- {author}</em></p>
+          </div>
+        )}
       </header>
     </div>
   );
 }
-
 export default App;
